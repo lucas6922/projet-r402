7d073cbf7fb8f412316d2ec9ad895eeb2360684d
lucas <lucas.lebon75@gmail.com>
2025-04-02 17:12:20 +0200

    reset data

--- app/src/test/java/com/but/parkour/openapitools/client/apis/ConnexionApiTest.kt
+++ app/src/test/java/com/but/parkour/openapitools/client/apis/ConnexionApiTest.kt
@@ -1,8 +1,10 @@
 package com.but.parkour.openapitools.client.apis
 
 import com.but.parkour.BuildConfig
+import com.but.parkour.clientkotlin.apis.CompetitionsApi
 import com.but.parkour.clientkotlin.apis.UtilApi
 import com.but.parkour.clientkotlin.infrastructure.ApiClient
+import com.but.parkour.clientkotlin.models.CompetitionUpdate
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -13,6 +15,8 @@ class ConnexionApiTest {
     private val apiService = apiClient.createService(UtilApi::class.java)
     private val call = apiService.resetData()
 
+    private val competitionApi = apiClient.createService(CompetitionsApi::class.java)
+
 
     @Test
     fun resetDataTest() {
@@ -22,6 +26,36 @@ class ConnexionApiTest {
 
         assertEquals(200, code)
         assertEquals(null, erreur)
+
+        //mettre une competition en finished
+
+        val competitions = competitionApi.getAllCompetitions().execute().body()
+
+        val competition0 = competitions?.get(0)
+
+        val competitionUpdate = CompetitionUpdate(
+            status = CompetitionUpdate.Status.finished
+        )
+
+        competitionApi.updateCompetition(competition0?.id!!, competitionUpdate).execute()
+
+        val competition1 = competitions.get(1)
+
+        val competitionUpdate1 = CompetitionUpdate(
+            status = CompetitionUpdate.Status.not_started
+        )
+
+        competitionApi.updateCompetition(competition1.id!!, competitionUpdate1).execute()
+
+        val competition2 = competitions.get(2)
+
+        val competitionUpdate2 = CompetitionUpdate(
+            status = CompetitionUpdate.Status.started
+        )
+
+        competitionApi.updateCompetition(competition2.id!!, competitionUpdate2).execute()
+
+
     }
 
 }
